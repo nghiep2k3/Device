6229678e30851e535e6042678803d562d26a2a8f
RyuuMugen <thangcp8@gmail.com>
2023-04-07 18:55:36 +0700

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client"
 import './assets/styles/index.css';
 import Login from './pages/Authorization/login.jsx';
 import Change from './pages/Profile/components/ChangePassWord.jsx';
+import Update from './pages/Profile/components/Update.jsx';
 import Menus from './pages/Profile/menu.jsx';
 import ListUser from './pages/Profile/components/Profile.jsx';
 import Edit from './pages/Profile/User/Edit.jsx';
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="ChangePassWord" element={<Change />} />
           <Route path="Menu" element={<Menus />} />
           <Route path="ListUser" element={<ListUser />} />
+          <Route path="Update" element={<Update />} />
           <Route path="Create" element={<Create />} />
           <Route path="Edit" element={<Edit />} />
         </Route>
