5985eeabea70ffc370be193d614ffbec81257215
Nguyễn Nghiệp <nghiep2k3>
2023-05-15 22:36:38 +0700

    code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import ReactDOM from "react-dom/client"
 import './assets/styles/index.css';
+import DeviceManager from './components/DeviceManager/DeviceManager.jsx'
+import CreateDevice from './components/CreateDevice/CreateDevice.jsx'
 
 import router from './Router';
 import { RouterProvider } from 'react-router-dom';
@@ -9,7 +11,10 @@ export default function App() {
 
   return (
     <RouterProvider router={router}></RouterProvider>
-
+    // <div>
+    //   <DeviceManager/>
+    //   <CreateDevice />
+    // </div>
   );
 }
 
